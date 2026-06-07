794bb2ed6f7db9d59a32c353509db4388357a6da
Martin Bartos <brtsmrtn@gmail.com>
2026-06-07 19:18:43 +0200

    close 03/ng-di-token

--- chapter03/start_here/ng-di-token/src/app/services/user.service.ts
+++ chapter03/start_here/ng-di-token/src/app/services/user.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@angular/core';
-import { Greeter } from '../classes/greeter.class';
+import { inject, Injectable } from '@angular/core';
+import { GREETER_TOKEN } from '../classes/greeter.class';
 
 @Injectable({
   providedIn: 'root'
@@ -18,10 +18,10 @@ export class UserService {
     firstName: 'Peter',
     lastName: 'Parker'
   }]
-  constructor() { }
+  #greeter = inject(GREETER_TOKEN);
 
   getUser() {
     const user = this.users[Math.floor(Math.random() * this.users.length)]
-    return new Greeter(user);
+    return new this.#greeter(user);
   }
 }
